0de14c12abad4b684b6efe14d43f6acb3536bba2
Bran Liang <lby89757@hotmail.com>
2022-06-19 21:29:32 +0800

    Implement unique snowflakes

--- 00/food_lines.c
+++ 00/food_lines.c
@@ -1,34 +1,40 @@
 #define MAX_LINES 100
 #include <stdio.h>
 
-int shortest_line_index(int lines[], int n) {
+int shortest_line_index(int lines[], int n)
+{
     int j;
     int shortest = 0;
-    for (j = 0; j < n; j++) {
-        if (lines[j] < lines[shortest]) {
+    for (j = 0; j < n; j++)
+    {
+        if (lines[j] < lines[shortest])
+        {
             shortest = j;
         }
     }
     return shortest;
 }
 
-void solve(int lines[], int n, int m) {
+void solve(int lines[], int n, int m)
+{
     int i, shortest;
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < m; i++)
+    {
         shortest = shortest_line_index(lines, n);
         printf("%d\n", lines[shortest]);
         lines[shortest]++;
     }
 }
 
-int main(void) {
+int main(void)
+{
     int lines[MAX_LINES];
     int n, m, i;
     scanf("%d%d", &n, &m);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &lines[i]);
     }
     solve(lines, n, m);
     return 0;
 }
-
---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithmic thinking exercises
+
+- [00 food line](https://dmoj.ca/problem/lkp18c2p1)
