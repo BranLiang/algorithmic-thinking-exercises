9158998428d30f76bf01ef66aa1870ca46877d79
Bran Liang <lby89757@hotmail.com>
2022-07-02 09:11:49 +0800

    Optimize the mice maze using only one run

--- 13/mice_maze.c
+++ 13/mice_maze.c
@@ -10,10 +10,11 @@ typedef struct passage
     struct passage *next;
 } passage;
 
-int find_min_time(int from_cell, int exit_cell, passage *cells[], int cells_num)
+int find_min_times(int from_cell, int time_limit, passage *cells[], int cells_num)
 {
     static int fixed[MAX_CELLS] = {0};
     static int min_times[MAX_CELLS];
+    int total = 0;
     int i, j, min_time, min_time_index, found;
     passage *e;
     for (i = 1; i <= cells_num; i++)
@@ -46,6 +47,11 @@ int find_min_time(int from_cell, int exit_cell, passage *cells[], int cells_num)
             break;
         }
 
+        if (min_time <= time_limit)
+        {
+            total += 1;
+        }
+        
         fixed[min_time_index] = 1;
         e = cells[min_time_index];
         while (e)
@@ -62,7 +68,7 @@ int find_min_time(int from_cell, int exit_cell, passage *cells[], int cells_num)
             e = e->next;
         }
     }
-    return min_times[exit_cell];
+    return total;
 }
 
 int main(void)
@@ -84,20 +90,12 @@ int main(void)
         for (j = 0; j < passages_num; j++)
         {
             e = malloc(sizeof(passage));
-            scanf("%d %d %d", &from_cell, &e->to_cell, &e->time);
+            scanf("%d %d %d", &e->to_cell, &from_cell, &e->time);
             e->next = cells[from_cell];
             cells[from_cell] = e;
         }
 
-        total = 0;
-        for (j = 1; j <= cells_num; j++)
-        {
-            min_time = find_min_time(j, exit_cell, cells, cells_num);
-            if (min_time >= 0 && min_time <= time_limit)
-            {
-                total += 1;
-            }
-        }
+        total = find_min_times(exit_cell, time_limit,  cells, cells_num);
         printf("%d\n", total);
         if (i < cases_num)
         {
