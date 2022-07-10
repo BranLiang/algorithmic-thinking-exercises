1b6b538bf1fc60cbcdbaec746636a86f26fe67b7
Bran Liang <lby89757@hotmail.com>
2022-07-10 20:28:51 +0800

    Add segment tree to boost performance for problem 20

--- 20/building_treaps.c
+++ 20/building_treaps.c
@@ -43,21 +43,82 @@ int comparer(const void *v1, const void *v2)
     return strcmp(n1->label, n2->label);
 }
 
-int max_priority_index(treap_node treap_nodes[], int left, int right)
+typedef struct
 {
-    int i;
-    int max_index = left;
-    for (i = left + 1; i <= right; i++)
+    int left, right;
+    int max_index;
+} segtree_node;
+
+
+void init_segtree(segtree_node segtree[], int node, int left, int right)
+{
+    int mid;
+    segtree[node].left = left;
+    segtree[node].right = right;
+    if (left == right)
     {
-        if (treap_nodes[i].priority > treap_nodes[max_index].priority)
-        {
-            max_index = i;
-        }
+        return;
     }
-    return max_index;
+    mid = (left + right) / 2;
+    init_segtree(segtree, node * 2, left, mid);
+    init_segtree(segtree, node * 2 + 1, mid + 1, right);
 }
 
-void solve(treap_node treap_nodes[], int left, int right)
+int fill_segtree(segtree_node segtree[], int node, treap_node treap_nodes[])
+{
+    int left_max, right_max;
+    if (segtree[node].left == segtree[node].right)
+    {
+        segtree[node].max_index = segtree[node].left;
+        return segtree[node].max_index;
+    }
+    left_max = fill_segtree(segtree, node * 2, treap_nodes);
+    right_max = fill_segtree(segtree, node * 2 + 1, treap_nodes);
+    if (treap_nodes[left_max].priority > treap_nodes[right_max].priority)
+    {
+        segtree[node].max_index = left_max;
+    }
+    else
+    {
+        segtree[node].max_index = right_max;
+    }
+    return segtree[node].max_index;
+}
+
+int query_segtree(segtree_node segtree[], int node, treap_node treap_nodes[], int left, int right)
+{
+    int left_max, right_max;
+    if (right < segtree[node].left || left > segtree[node].right)
+    {
+        return -1;
+    }
+    if (right >= segtree[node].right && left <= segtree[node].left)
+    {
+        return segtree[node].max_index;
+    }
+    left_max = query_segtree(segtree, node * 2, treap_nodes, left, right);
+    right_max = query_segtree(segtree, node * 2 + 1, treap_nodes, left, right);
+    if (left_max == -1)
+    {
+        return right_max;
+    }
+
+    if (right_max == -1)
+    {
+        return left_max;
+    }
+
+    if (treap_nodes[left_max].priority > treap_nodes[right_max].priority)
+    {
+        return left_max;
+    }
+    else
+    {
+        return right_max;
+    }
+}
+
+void solve(segtree_node segtree[], treap_node treap_nodes[], int left, int right)
 {
     int root_index;
     treap_node root;
@@ -65,18 +126,19 @@ void solve(treap_node treap_nodes[], int left, int right)
     {
         return;
     }
-    root_index = max_priority_index(treap_nodes, left, right);
+    root_index = query_segtree(segtree, 1, treap_nodes, left, right);
     root = treap_nodes[root_index];
     printf("(");
-    solve(treap_nodes, left, root_index - 1);
+    solve(segtree, treap_nodes, left, root_index - 1);
     printf("%s/%d", root.label, root.priority);
-    solve(treap_nodes, root_index + 1, right);
+    solve(segtree, treap_nodes, root_index + 1, right);
     printf(")");
 }
 
 int main(void)
 {
     static treap_node treap_nodes[MAX_NODES];
+    static segtree_node segtree[MAX_NODES * 4 + 1];
     int node_num, priority;
     char *label;
     scanf("%d ", &node_num);
@@ -88,7 +150,9 @@ int main(void)
             scanf("%d ", &treap_nodes[i].priority);
         }
         qsort(treap_nodes, node_num, sizeof(treap_node), comparer);
-        solve(treap_nodes, 0, node_num - 1);
+        init_segtree(segtree, 1, 0, node_num - 1);
+        fill_segtree(segtree, 1, treap_nodes);
+        solve(segtree, treap_nodes, 0, node_num - 1);
         printf("\n");
         scanf("%d ", &node_num);
     }
