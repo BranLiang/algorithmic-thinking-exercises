b69eaa995f5e5f662948fb7df1cddd957e7b6ba6
Bran Liang <lby89757@hotmai.com>
2022-10-15 11:13:17 +0800

    Update 27_rust

--- 27/main.c
+++ 27/main.c
@@ -9,85 +9,195 @@ typedef struct node {
     struct node *next;
 } node;
 
+typedef struct {
+    int from;
+    int to;
+    int distance;
+} route;
+
 void add_node(int from, int to, int weight, node *tree[MAX_N])
 {
     node *new_node = malloc(sizeof(node));
     new_node->weight = weight;
     new_node->to = to;
     new_node->next = tree[from];
-    tree[from] = new_node;    
+    tree[from] = new_node;
 }
 
-void find_longest_path(int start, node *tree[MAX_N], int *longest, int *second_longest)
+void remove_node(node *tree[MAX_N], int from, node *target)
 {
-    int current_size, new_size;
-    int current_locations[MAX_N], new_locations[MAX_N];
-    int distances[MAX_N] = {[0 ... MAX_N - 1] = -1};
+    node *prev = NULL;
+    node *cur = tree[from];
+    while (cur != NULL) {
+        if (cur == target) {
+            if (prev == NULL) {
+                tree[from] = cur->next;
+            } else {
+                prev->next = cur->next;
+            }
+            return;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+}
 
-    distances[start] = 0;
-    current_size = 1;
-    new_size = 0;
-    current_locations[0] = start;
+route max_route_single_direction(int from, node *tree[MAX_N])
+{
+    route max;
+    max.distance = 0;
+    max.from = from;
+    max.to = from;
 
-    while (current_size > 0)
+    node *current = tree[from];
+    while (current != NULL) {
+        route current_route = max_route_single_direction(current->to, tree);
+        int distance = current->weight + current_route.distance;
+        if (distance > max.distance) {
+            max.distance = distance;
+            max.to = current_route.to;
+        }
+        current = current->next;
+    }
+    return max;
+}
+
+route max_route(int through, node *tree[MAX_N])
+{
+    route first;
+    first.distance = 0;
+    first.from = through;
+    first.to = through;
+
+    route second;
+    second.distance = 0;
+    second.from = through;
+    second.to = through;
+
+    node *current = tree[through];
+    while (current != NULL) {
+        route current_route = max_route_single_direction(current->to, tree);
+        int distance = current->weight + current_route.distance;
+        if (distance > first.distance) {
+            second.distance = first.distance;
+            second.to = first.to;
+            first.distance = distance;
+            first.to = current_route.to;
+        } else if (distance > second.distance) {
+            second.distance = distance;
+            second.to = current_route.to;
+        }
+        current = current->next;
+    }
+
+    route max;
+    max.distance = first.distance + second.distance;
+    max.from = second.to;
+    max.to = first.to;
+
+    return max;
+}
+
+int include_node(int from, int to, node *tree[MAX_N])
+{
+    node *current = tree[from];
+    while (current != NULL) {
+        if (current->to == to || include_node(current->to, to, tree)) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+int find_edge(node *tree[MAX_N], int from, int to)
+{
+    node *current = tree[from];
+    while (current != NULL) {
+        if (current->to == to || include_node(current->to, to, tree)) {
+            return current->to;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+route find_max_route(node *tree[MAX_N])
+{
+    route max;
+
+    for (int i = 1; i < MAX_N; i++)
     {
-        for (int i = 0; i < current_size; i++)
-        {
-            node *location = tree[current_locations[i]];
-            while (location != NULL)
-            {
-                int to = location->to;
-                if (distances[to] == -1)
-                {
-                    int new_distance = distances[current_locations[i]] + location->weight;
-                    if (new_distance >= *longest && to > start)
-                    {
-                        *second_longest = *longest;
-                        *longest = new_distance;
-                    }
-                    else if (new_distance > *second_longest && to > start)
-                    {
-                        *second_longest = new_distance;
-                    }
-                    
-                    distances[to] = new_distance;
-                    new_locations[new_size] = to;
-                    new_size++;
-                }
-                location = location->next;
+        if (tree[i] != NULL) {
+            route current = max_route(i, tree);
+            if (current.distance > max.distance) {
+                max = current;
             }
         }
-        current_size = new_size;
-        new_size = 0;
-        for (int j = 0; j < current_size; j++)
-        {
-            current_locations[j] = new_locations[j];
-        }
     }
+    return max;
+}
+
+void solve(node *tree[MAX_N], int reverse_tree[MAX_N])
+{
+    route max = find_max_route(tree);
+
+    printf("from: %d, to: %d, distance: %d\n", max.from, max.to, max.distance);
+
+    // int to = find_edge(tree, max.from, max.to);
+
+    // printf("node, from: %d, to: %d", max.from, to);
+
+    // node *node1, *node2;
+    // route max1, max2;
+    // int from;
+
+    // if (reverse_tree[max.from] == 0)
+    // {
+    //     node1 = find_start(tree, max);
+    //     remove_node(tree, max.from, node1);
+    //     max1 = find_max_route(tree);
+    //     add_node(max.from, node1->to, node1->weight, tree);
+    // } else {
+    //     from = reverse_tree[max.from];
+    //     node1 = tree[from];
+    //     remove_node(tree, from, node1);
+    //     max1 = find_max_route(tree);
+    //     add_node(from, node1->to, node1->weight, tree);
+    // }
+
+    // from = reverse_tree[max.to];
+    // node2 = tree[from];
+    // remove_node(tree, from, node2);
+    // max2 = find_max_route(tree);
+
+    // if (max1.distance > max2.distance) {
+    //     printf("%d", max1.distance);
+    // } else {
+    //     printf("%d", max2.distance);
+    // }
 }
 
 int main(void)
 {
-    int longest, second_longest;
-    longest = 0;
-    second_longest = 0;
+    node *tree[MAX_N] = {NULL};
+    int reverse_tree[MAX_N];
 
-    int n, a, b, weight;
+    for (int i = 0; i < MAX_N; i++)
+    {
+        reverse_tree[i] = 0;
+    }
+
+    int n;
     scanf("%d", &n);
-    static node *tree[MAX_N] = {NULL};
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i < n; i++)
     {
-        scanf("%d %d %d", &a, &b, &weight);
-
-        add_node(a, b, weight, tree);
-        add_node(b, a, weight, tree);
+        int from, to, weight;
+        scanf("%d %d %d", &from, &to, &weight);
+        reverse_tree[to] = from;
+        add_node(from, to, weight, tree);
     }
 
-    for (int i = 1; i <= n; i++)
-    {
-        find_longest_path(i, tree, &longest, &second_longest);
-    }
-
-    printf("%d\n", second_longest);
+    solve(tree, reverse_tree);
 }
--- 27_rust/src/main.rs
+++ 27_rust/src/main.rs
@@ -1,159 +1,264 @@
-use std::io;
+use std::io::{self, StdinLock};
+use std::io::BufRead;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone, Copy)]
-struct Path {
+type Map = HashMap<usize, Vec<Edge>>;
+
+#[derive(Debug)]
+struct Edge {
     to: usize,
-    distance: usize,
+    length: usize,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug)]
 struct Route {
-    from: usize,
     to: usize,
+    through: usize,
     distance: usize,
 }
 
-fn longest_route_to_leaf(from: usize, paths: &HashMap<usize, Vec<Path>>, cache: &mut HashMap<usize, Route>) -> Route {
-    if let Some(&route) = cache.get(&from) {
-        return route;
-    }
+fn parse(line: &str) -> (usize, Edge) {
+    let mut parts = line.split_whitespace();
 
-    let mut max = Route {
-        from: from,
-        to: from,
-        distance: 0,
-    };
-    for path in paths.get(&from).unwrap() {
-        let route = longest_route_to_leaf(path.to, paths, cache);
-        let distance = path.distance + route.distance;
-        if distance > max.distance {
-            max.distance = distance;
-            max.to = route.to;
-        }
-    }
+    let from = parts.next().unwrap().parse::<usize>().unwrap();
+    let to = parts.next().unwrap().parse::<usize>().unwrap();
+    let length = parts.next().unwrap().parse::<usize>().unwrap();
 
-    cache.insert(from, max.clone());
-    max
-}
-
-fn longest_route(through: usize, paths: &HashMap<usize, Vec<Path>>, cache: &mut HashMap<usize, Route>) -> Route {
-    let mut routes = Vec::new();
-    for path in paths.get(&through).unwrap() {
-        let route = longest_route_to_leaf(path.to, paths, cache);
-        routes.push(Route {
-            from: through,
-            to: route.to,
-            distance: path.distance + route.distance,
-        });
-    }
-
-    if routes.len() < 1 {
-        return Route {
-            from: through,
-            to: through,
-            distance: 0,
-        };
-    }
-    
-    if routes.len() == 1 {
-        routes[0]
+    if from < to {
+        (from, Edge { to, length })
     } else {
-        routes.sort_by(|a, b| b.distance.cmp(&a.distance));
-        Route {
-            from: routes[0].to,
-            to: routes[1].to,
-            distance: routes[0].distance + routes[1].distance,
-        }
+        (to, Edge { to: from, length })
     }
 }
 
-fn find_longest_route(n: usize, paths: &HashMap<usize, Vec<Path>>) -> Route {
-    let mut cache = HashMap::new();
-    let mut max = Route {
-        from: 0,
-        to: 0,
-        distance: 0,
-    };
-    for i in 1..=n {
-        let route = longest_route(i, paths, &mut cache);
-        if route.distance > max.distance {
-            max = route;
-        }
+fn load(handle: &mut StdinLock, edges: &mut Map, parents: &mut HashMap<usize, usize>) {
+    for line in handle.lines() {
+        let line = line.unwrap();
+        let (from, edge) = parse(&line);
+        let to = edge.to;
+        edges.entry(from).or_insert(Vec::new()).push(edge);
+        parents.insert(to, from);
     }
-    max
 }
 
-fn remove_path(paths: &mut HashMap<usize, Vec<Path>>, from: Option<usize>, to: usize) {
+fn remove_edge(from: usize, to: usize, edges: &mut Map) -> usize {
     let mut i = 0;
-    match from {
-        Some(from) => {
-            let paths = paths.get_mut(&from).unwrap();
-            while i < paths.len() {
-                if paths[i].to == to {
-                    paths.remove(i);
-                    break;
+    let mut length = 0;
+    for edge in edges.get_mut(&from).unwrap() {
+        if edge.to == to {
+            length = edge.length;
+            edges.get_mut(&from).unwrap().remove(i);
+            break;
+        }
+        i += 1;
+    }
+    length
+}
+
+fn add_edge(from: usize, to: usize, length: usize, edges: &mut Map) {
+    edges.entry(from).or_insert(Vec::new()).push(Edge { to, length });
+}
+
+fn find_longest_wing_distance(from: usize, edges: &Map) -> (usize, usize) {
+    let mut distance = 0;
+    let mut to = from;
+    match edges.get(&from) {
+        Some(to_edges) => {
+            for edge in to_edges {
+                let (to2, distance2) = find_longest_wing_distance(edge.to, edges);
+                if distance2 + edge.length > distance {
+                    distance = distance2 + edge.length;
+                    to = to2;
                 }
-                i += 1;
             }
         },
-        None => {
-            paths.get_mut(&to).unwrap().clear();
-            paths.entry(to).or_insert(Vec::new());
-        }
+        None => (),
     }
+    (to, distance)
 }
 
+fn find_longest(from: usize, edges: &Map, parents: &HashMap<usize, usize>, paths: &mut Vec<(usize, usize)>, fill_paths: bool) -> usize {
+    let mut longest = Route { to: from, through: from, distance: 0 };
+    let mut second_longest = Route { to: from, through: from, distance: 0 };
 
-fn solve(n: usize, paths: &mut HashMap<usize, Vec<Path>>, lookup: &HashMap<usize, usize>, cloned_paths: &mut HashMap<usize, Vec<Path>>) {
-    let route = find_longest_route(n, paths);
-
-    match lookup.get(&route.from) {
-        Some(&from) => remove_path(paths, Some(from), route.from),
-        // If not found, it's the root node
-        None => remove_path(paths, None, route.from),
+    match edges.get(&from) {
+        Some(to_edges) => {
+            for edge in to_edges {
+                let (to, distance) = find_longest_wing_distance(edge.to, edges);
+                if distance + edge.length > longest.distance {
+                    second_longest = longest;
+                    longest = Route { to: to, through: edge.to, distance: distance + edge.length };
+                } else if distance + edge.length > second_longest.distance {
+                    second_longest = Route { to: to, through: edge.to, distance: distance + edge.length };
+                }
+            }
+        },
+        None => (),
     }
 
-    let route_a = find_longest_route(n, paths);
+    if fill_paths {
+        if second_longest.distance != 0 {
+            paths.push((*parents.get(&longest.to).unwrap(), longest.to));
+            paths.push((*parents.get(&second_longest.to).unwrap(), second_longest.to));
+        } else {
+            paths.push((from, longest.through));
+            paths.push((*parents.get(&longest.to).unwrap(), longest.to));
+        }
+    }
+    longest.distance + second_longest.distance
+}
 
-    match lookup.get(&route.to) {
-        Some(&to) => remove_path(cloned_paths, Some(to), route.to),
-        // If not found, it's the root node
-        None => remove_path(cloned_paths, None, route.to),
+fn find_second_longest(n: usize, edges: &mut Map, parents: &HashMap<usize, usize>) -> usize {
+    let mut longest = 0;
+    let mut tmp_paths = Vec::new();
+    let mut paths = Vec::new();
+    for from in 1..n {
+        let distance = find_longest(from, edges, parents, &mut tmp_paths, true);
+        if distance > longest {
+            paths.clear();
+            paths.push(tmp_paths.pop().unwrap());
+            paths.push(tmp_paths.pop().unwrap());
+            longest = distance;
+        }
     }
 
-    let route_b = find_longest_route(n, cloned_paths);
-    if route_a.distance > route_b.distance {
-        println!("{}", route_a.distance);
-    } else {
-        println!("{}", route_b.distance);
+    let mut second_longest = 0;
+    for (from, to) in paths {
+        let length = remove_edge(from, to, edges);
+        for from in 1..n {
+            let distance = find_longest(from, edges, parents, &mut tmp_paths, false);
+            if distance > second_longest {
+                second_longest = distance;
+            }
+        }
+        add_edge(from, to, length, edges);
     }
+    second_longest
 }
 
 fn main() -> io::Result<()> {
+    let mut edges = HashMap::new();
+    let mut parents = HashMap::new();
+
+    let mut handle = io::stdin().lock();
+
+    // Read N
     let mut buf = String::new();
-    let mut paths = HashMap::new();
-    let mut cloned_paths = HashMap::new();
-    let mut lookup = HashMap::new();
+    handle.read_line(&mut buf).unwrap();
+    let n = buf.trim().parse::<usize>().unwrap();
 
-    io::stdin().read_line(&mut buf)?;
-    let n: usize = buf.trim().parse().unwrap();
-
-    for _ in 1..n {
-        buf.clear();
-        io::stdin().read_line(&mut buf)?;
-        let mut iter = buf.split_whitespace();
-        let from: usize = iter.next().unwrap().parse().unwrap();
-        let to: usize = iter.next().unwrap().parse().unwrap();
-        let distance: usize = iter.next().unwrap().parse().unwrap();
-        paths.entry(from).or_insert(Vec::new()).push(Path { to, distance });
-        paths.entry(to).or_insert(Vec::new());
-        cloned_paths.entry(from).or_insert(Vec::new()).push(Path { to, distance });
-        cloned_paths.entry(to).or_insert(Vec::new());
-        lookup.insert(to, from);
-    }
-
-    solve(n, &mut paths, &lookup, &mut cloned_paths);
+    // Load edges and parents
+    load(&mut handle, &mut edges, &mut parents);
+    
+    println!("{}", find_second_longest(n, &mut edges, &parents));
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let line = "1 2 3";
+        let (from, edge) = parse(line);
+        assert_eq!(from, 1);
+        assert_eq!(edge.to, 2);
+        assert_eq!(edge.length, 3);
+    }
+
+    #[test]
+    fn test_remove_edge() {
+        let mut edges = HashMap::new();
+        edges.insert(1, vec![Edge { to: 2, length: 1 }]);
+        edges.insert(2, vec![Edge { to: 3, length: 1 }]);
+        let n = remove_edge(1, 2, &mut edges);
+        assert_eq!(n, 1);
+        assert_eq!(edges.get(&1).unwrap().len(), 0);
+        assert_eq!(edges.get(&2).unwrap().len(), 1);
+    }
+
+    #[test]
+    fn test_find_longest_wing_distance() {
+        let mut edges = HashMap::new();
+        add_edge(1, 2, 1, &mut edges);
+        add_edge(1, 3, 2, &mut edges);
+        add_edge(2, 4, 2, &mut edges);
+        let (to, distance) = find_longest_wing_distance(1, &edges);
+        assert_eq!(to, 4);
+        assert_eq!(distance, 3);
+    }
+
+    #[test]
+    fn test_find_longest() {
+        let mut edges = HashMap::new();
+        add_edge(1, 2, 1, &mut edges);
+        add_edge(1, 3, 2, &mut edges);
+        add_edge(2, 4, 2, &mut edges);
+        add_edge(4, 5, 1, &mut edges);
+        let mut parents = HashMap::new();
+        let mut paths = Vec::new();
+        parents.insert(2, 1);
+        parents.insert(3, 1);
+        parents.insert(4, 2);
+        parents.insert(5, 4);
+        let distance = find_longest(1, &edges, &parents, &mut paths, true);
+        assert_eq!(distance, 6);
+        assert_eq!(paths.len(), 2);
+        assert_eq!(paths[0], (4, 5));
+        assert_eq!(paths[1], (1, 3));
+    }
+
+    #[test]
+    fn test_find_longest_with_single_route() {
+        let mut edges = HashMap::new();
+        add_edge(1, 2, 1, &mut edges);
+        add_edge(2, 3, 2, &mut edges);
+        add_edge(3, 4, 2, &mut edges);
+        add_edge(4, 5, 1, &mut edges);
+        let mut parents = HashMap::new();
+        let mut paths = Vec::new();
+        parents.insert(2, 1);
+        parents.insert(3, 2);
+        parents.insert(4, 3);
+        parents.insert(5, 4);
+        let distance = find_longest(2, &edges, &parents, &mut paths, true);
+        assert_eq!(distance, 5);
+        assert_eq!(paths.len(), 2);
+        assert_eq!(paths[0], (2, 3));
+        assert_eq!(paths[1], (4, 5));
+    }
+
+    #[test]
+    fn test_find_second_longest() {
+        let mut edges = HashMap::new();
+        add_edge(1, 2, 1, &mut edges);
+        add_edge(1, 3, 2, &mut edges);
+        add_edge(2, 4, 2, &mut edges);
+        add_edge(4, 5, 1, &mut edges);
+        let mut parents = HashMap::new();
+        parents.insert(2, 1);
+        parents.insert(3, 1);
+        parents.insert(4, 2);
+        parents.insert(5, 4);
+        let distance = find_second_longest(5, &mut edges, &parents);
+        assert_eq!(distance, 4);
+    }
+
+    #[test]
+    fn test_find_second_longest_with_single_line() {
+        let mut edges = HashMap::new();
+        add_edge(1, 2, 1, &mut edges);
+        add_edge(2, 3, 2, &mut edges);
+        add_edge(3, 4, 3, &mut edges);
+        add_edge(4, 5, 4, &mut edges);
+        let mut parents = HashMap::new();
+        parents.insert(2, 1);
+        parents.insert(3, 2);
+        parents.insert(4, 3);
+        parents.insert(5, 4);
+        let distance = find_second_longest(5, &mut edges, &parents);
+        assert_eq!(distance, 9);
+    }
 }
