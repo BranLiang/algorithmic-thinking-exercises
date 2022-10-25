fc253f4372ad4a06c31e63934a8ffa38345bb69f
Bran Liang <lby89757@hotmai.com>
2022-10-25 22:38:47 +0800

    Solve problem 31 wish upon a star

--- 31_rust/src/main.rs
+++ 31_rust/src/main.rs
@@ -16,46 +16,30 @@ fn parse_edge() -> (usize, usize) {
     let mut iter = input.split_whitespace();
     let from = iter.next().unwrap().parse().unwrap();
     let to = iter.next().unwrap().parse().unwrap();
-    if from < to {
-        (from, to)
-    } else {
-        (to, from)
-    }
+    (from, to)
 }
 
-fn find_edges(graph: &HashMap<usize, Vec<usize>>, node: usize) -> Vec<usize> {
-    let mut edges = Vec::new();
-    if !graph.contains_key(&node) {
-        edges.push(node);
-        return edges;
-    }
-    for parent in graph.get(&node).unwrap() {
-        edges.extend(&find_edges(graph, *parent));
-    }
-    edges
-}
+fn connected(graph: &HashMap<usize, Vec<usize>>, node1: usize, node2: usize) -> bool {
+    let mut visited = HashSet::new();
 
-fn connected(graph1: &HashMap<usize, Vec<usize>>, graph2: &HashMap<usize, Vec<usize>>, node1: usize, node2: usize) -> bool {
-    let edges1 = find_edges(graph1, node1);
-    let edges2 = find_edges(graph1, node2);
-    let edges3 = find_edges(graph2, node1);
-    let edges4 = find_edges(graph2, node2);
-
-    let mut set1 = HashSet::new();
-    let mut set2 = HashSet::new();
-    for edge in edges1 {
-        set1.insert(edge);
+    let mut current = vec![node1];
+    while !current.is_empty() {
+        let mut next = vec![];
+        for node in current {
+            if node == node2 {
+                return true;
+            }
+            if visited.contains(&node) {
+                continue;
+            }
+            visited.insert(node);
+            if let Some(neighbors) = graph.get(&node) {
+                next.extend(neighbors);
+            }
+        }
+        current = next;
     }
-    for edge in edges3 {
-        set1.insert(edge);
-    }
-    for edge in edges2 {
-        set2.insert(edge);
-    }
-    for edge in edges4 {
-        set2.insert(edge);
-    }
-    set1.intersection(&set2).count() > 0
+    false
 }
 
 fn main() {
@@ -66,29 +50,24 @@ fn main() {
         return;
     }
     
-    let mut to_root_graph: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut to_leaf_graph: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut nodes: HashSet<usize> = HashSet::new();
-
-    let mut counter = 0;
+    let mut graph = HashMap::new();
+    let mut threshold = 0;
 
     for _ in 0..m {
         let (from, to) = parse_edge();
 
-        if nodes.contains(&from) && nodes.contains(&to) {
-            if connected(&to_root_graph, &to_leaf_graph, from, to) {
-                counter += 1;
-                if counter > 1 {
+        if graph.contains_key(&from) && graph.contains_key(&to) {
+            if connected(&graph, from, to) {
+                threshold += 1;
+                if threshold > 1 {
                     println!("NO");
                     return;
                 }
             }
         }
 
-        nodes.insert(from);
-        nodes.insert(to);
-        to_root_graph.entry(to).or_insert(Vec::new()).push(from);
-        to_leaf_graph.entry(from).or_insert(Vec::new()).push(to);
+        graph.entry(to).or_insert(Vec::new()).push(from);
+        graph.entry(from).or_insert(Vec::new()).push(to);
     }
     println!("YES");
 }
@@ -98,25 +77,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_find_edges() {
-        let mut graph: HashMap<usize, Vec<usize>> = HashMap::new();
-        graph.entry(2).or_insert(Vec::new()).push(1);
-        graph.entry(5).or_insert(Vec::new()).push(2);
-        graph.entry(5).or_insert(Vec::new()).push(4);
-        graph.entry(4).or_insert(Vec::new()).push(3);
-        graph.entry(2).or_insert(Vec::new()).push(6);
-        let roots = find_edges(&graph, 5);
-        assert_eq!(roots.len(), 3);
-        assert!(roots.contains(&1));
-        assert!(roots.contains(&3));
-        assert!(roots.contains(&6));
+    fn test_not_connected() {
+        let mut graph = HashMap::new();
+        link(&mut graph, 1, 2);
+        link(&mut graph, 3, 4);
+        assert!(!connected(&graph, 1, 3));
     }
 
     #[test]
-    fn test_find_edges_with_no_parents() {
-        let graph: HashMap<usize, Vec<usize>> = HashMap::new();
-        let roots = find_edges(&graph, 5);
-        assert_eq!(roots.len(), 1);
-        assert!(roots.contains(&5));
+    fn test_connected() {
+        let mut graph = HashMap::new();
+        link(&mut graph, 1, 2);
+        link(&mut graph, 2, 3);
+        link(&mut graph, 3, 4);
+        assert!(connected(&graph, 1, 4));
+    }
+
+    fn link(graph: &mut HashMap<usize, Vec<usize>>, from: usize, to: usize) {
+        graph.entry(from).or_insert(Vec::new()).push(to);
+        graph.entry(to).or_insert(Vec::new()).push(from);
     }
 }
