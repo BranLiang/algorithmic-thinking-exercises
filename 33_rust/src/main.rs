6551bad36709d57c5f3d8b9b9f00d44c60a35db0
Bran Liang <lby89757@hotmai.com>
2022-11-09 21:42:00 +0800

    Add solution for mimi

--- 33_rust/src/main.rs
+++ 33_rust/src/main.rs
@@ -10,18 +10,41 @@ fn is_palindrome(s: &str) -> bool {
     true
 }
 
+fn maybe_palindrome(len: usize, sums: &Vec<usize>) -> bool {
+    let left = sums[len / 2];
+    if len % 2 == 0 {
+        let right = sums[len] - sums[len / 2];
+        left == right
+    } else {
+        let right = sums[len] - sums[len / 2 + 1];
+        left == right
+    }
+}
+
 fn existed(s: &str, len: usize) -> bool {
     s[1..].contains(&s[..len])
+}
+
+fn substring_sums(s: &str) -> Vec<usize> {
+    let mut sums = Vec::new();
+    sums.push(0);
+    for (i, c) in s.chars().enumerate() {
+        sums.push(sums[i] + c as usize - 'a' as usize + 1);
+    }
+    sums
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+
+    let sums = substring_sums(&input);
 
     for i in (1..input.len()).rev() {
         let s = &input[0..i];
         
-        if is_palindrome(s) {
+        if maybe_palindrome(i, &sums) && is_palindrome(s) {
             if existed(&input, s.len()) {
                 println!("{}", s.len());
                 return;
@@ -47,5 +70,17 @@ mod tests {
         assert_eq!(existed("abab", 3), false);
         assert_eq!(existed("abab", 2), true);
         assert_eq!(existed("abab", 1), true);
+    }
+
+    #[test]
+    fn test_substring_sums() {
+        assert_eq!(substring_sums("abab"), vec![0, 1, 3, 4, 6]);
+    }
+
+    #[test]
+    fn test_maybe_parlindrome() {
+        assert_eq!(maybe_palindrome(1, &vec![0, 1, 3, 4, 6]), true);
+        assert_eq!(maybe_palindrome(2, &vec![0, 1, 3, 4, 6]), false);
+        assert_eq!(maybe_palindrome(3, &vec![0, 1, 3, 4, 6]), true);
     }
 }
