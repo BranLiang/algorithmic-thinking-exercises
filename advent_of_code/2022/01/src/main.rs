b80b6dea4351b9888b686b03237843eb97d460a1
branliang <bran.liang@shopify.com>
2022-12-01 16:47:59 +0800

    Update

--- advent_of_code/2022/01/src/main.rs
+++ advent_of_code/2022/01/src/main.rs
@@ -1,21 +1,21 @@
 use std::io;
 
 fn main() {
-    let mut max = 0;
+    let mut calories = Vec::new();
     let mut current = 0;
 
     for line in io::stdin().lines() {
         let line = line.unwrap();
         let line = line.trim();
         if line.is_empty() {
-            if current > max {
-                max = current;
-            }
+            calories.push(current);
             current = 0;
         } else {
             current += line.parse::<i32>().unwrap();
         }
     }
 
-    println!("{}", max);
+    calories.sort();
+    let sum: i32 = calories.iter().rev().take(3).sum();
+    println!("{}", sum);
 }
