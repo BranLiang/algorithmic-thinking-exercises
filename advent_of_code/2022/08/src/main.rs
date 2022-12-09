9e7ec40f6b6f93a151a60e227bf2c4fc40579e44
branliang <bran.liang@shopify.com>
2022-12-09 20:28:21 +0800

    Finish part 2 of day 8

--- advent_of_code/2022/08/src/main.rs
+++ advent_of_code/2022/08/src/main.rs
@@ -1,15 +1,73 @@
 use std::io;
-use std::cmp::max;
-use std::cmp::min;
 
 const SIZE: usize = 99;
 
+fn left(pos: (usize, usize), forest: &Vec<Vec<u32>>) -> u32 {
+    let (x, y) = pos;
+    let mut count = 0;
+
+    for j in (0..y).rev() {
+        if forest[x][j] < forest[x][y] {
+            count += 1;
+        }
+        if forest[x][j] >= forest[x][y] {
+            count += 1;
+            break;
+        }
+    }
+    count
+}
+
+fn right(pos: (usize, usize), forest: &Vec<Vec<u32>>, cols: usize) -> u32 {
+    let (x, y) = pos;
+    let mut count = 0;
+
+    for j in y+1..cols {
+        if forest[x][j] < forest[x][y] {
+            count += 1;
+        }
+        if forest[x][j] >= forest[x][y] {
+            count += 1;
+            break;
+        }
+    }
+    count
+}
+
+fn top(pos: (usize, usize), forest: &Vec<Vec<u32>>) -> u32 {
+    let (x, y) = pos;
+    let mut count = 0;
+
+    for i in (0..x).rev() {
+        if forest[i][y] < forest[x][y] {
+            count += 1;
+        }
+        if forest[i][y] >= forest[x][y] {
+            count += 1;
+            break;
+        }
+    }
+    count
+}
+
+fn bottom(pos: (usize, usize), forest: &Vec<Vec<u32>>, rows: usize) -> u32 {
+    let (x, y) = pos;
+    let mut count = 0;
+
+    for i in x+1..rows {
+        if forest[i][y] < forest[x][y] {
+            count += 1;
+        }
+        if forest[i][y] >= forest[x][y] {
+            count += 1;
+            break;
+        }
+    }
+    count
+}
+
 fn main() {
     let mut forest = Vec::new();
-    let mut left = [[0; SIZE]; SIZE];
-    let mut right = [[0; SIZE]; SIZE];
-    let mut top = [[0; SIZE]; SIZE];
-    let mut bottom = [[0; SIZE]; SIZE];
     let rows = SIZE;
     let cols = SIZE;
 
@@ -22,63 +80,22 @@ fn main() {
         forest.push(row);
     }
 
-    // left
-    for i in 0..rows {
-        left[i][0] = forest[i][0];
-    }
+    let mut max = 0;
 
-    for i in 0..rows {
-        for j in 1..cols {
-            left[i][j] = max(left[i][j - 1], forest[i][j]);
-        }
-    }
-
-    // right
-    for i in 0..rows {
-        right[i][cols - 1] = forest[i][cols - 1];
-    }
-
-    for i in 0..rows {
-        for j in (0..cols - 1).rev() {
-            right[i][j] = max(right[i][j + 1], forest[i][j]);
-        }
-    }
-
-    // top
-    for j in 0..cols {
-        top[0][j] = forest[0][j];
-    }
-
-    for j in 0..cols {
-        for i in 1..rows {
-            top[i][j] = max(top[i - 1][j], forest[i][j]);
-        }
-    }
-
-    // bottom
-    for j in 0..cols {
-        bottom[rows - 1][j] = forest[rows - 1][j];
-    }
-
-    for j in 0..cols {
-        for i in (0..rows - 1).rev() {
-            bottom[i][j] = max(bottom[i + 1][j], forest[i][j]);
-        }
-    }
-
-    let mut count = 0;
     for i in 1..rows - 1 {
         for j in 1..cols - 1 {
-            let min_height = min(
-                min(left[i][j - 1], right[i][j + 1]),
-                min(top[i - 1][j], bottom[i + 1][j])
-            );
-            if min_height < forest[i][j] {
-                count += 1;
+            let l = left((i, j), &forest);
+            let r = right((i, j), &forest, cols);
+            let t = top((i, j), &forest);
+            let b = bottom((i, j), &forest, rows);
+
+            let score = l * r * t * b;
+            if score > max {
+                max = score;
             }
         }
     }
 
-    let border = SIZE * SIZE - (SIZE - 2) * (SIZE - 2);
-    println!("{}", count + border);
+    println!("{}", max);
+
 }
